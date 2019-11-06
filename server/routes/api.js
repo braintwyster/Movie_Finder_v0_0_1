512b32fa0f737838b3c31e388d931b375bd075fe
Adam Yoder <pandasonicpdx@gmail.com>
2019-11-06 01:39:45 -0800

    Update api.js
    Making API a reuseable variable for easy app growth

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,5 @@
+const API_KEY = 'd1b727088c113dae3ece2952eac32c34'
+
 const express = require('express')
 const axios = require('axios')
 //Create Router Middleware access
@@ -5,31 +7,33 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     ///Get Data from MOVIE API
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=d1b727088c113dae3ece2952eac32c34')
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
     .then(d => {
         const results = d.data.results
         res.send(results)
+    })
+    .catch(err => {
+        console.log('ERR:', err);
     })
 })
 
 router.post('/search', (req, res) => {
     const query = encodeURI(req.body.title)
-    const url = 'https://api.themoviedb.org/3/search/movie?' +
-                'api_key=d1b727088c113dae3ece2952eac32c34' +
-                '&query=' + query
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
     ///Get Data from MOVIE API
     axios.get(url)
     .then( d => {
         const results = d.data.results
         res.send(results)
     })
+    .catch(err => {
+        console.log('ERR:', err);
+    })
 })
 
 router.get('/movie/:id', (req, res) =>{
     const id = Number(req.params.id)
-    const url = 'https://api.themoviedb.org/3/movie/' +
-                id +
-                '?api_key=d1b727088c113dae3ece2952eac32c34'
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
 
     ///Get Data from MOVIE API
     axios.get(url)
